e5eed11798064379bbd8a5a1ae802edd1c89894a
commoner <timothy.baron@gmail.com>
2014-10-29 10:29:39 -0700

    properly attribute scrollbarWidth

--- demo_moduiPopup.js
+++ demo_moduiPopup.js
@@ -12,6 +12,7 @@ var ExampleTwoModel = Backbone.Model.extend( {} );
 var exampleTwoModel = new ExampleTwoModel( { text: '' } );
 
 function scrollbarWidth() {
+	// thanks to http://chris-spittles.co.uk/jquery-calculate-scrollbar-width/
   var $inner = $('<div style="width: 100%; height:200px;">test</div>'),
       $outer = $('<div style="width:200px;height:150px; position: absolute; top: 0; left: 0; visibility: hidden; overflow:hidden;"></div>').append($inner),
       inner = $inner[0],
